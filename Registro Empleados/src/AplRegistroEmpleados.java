b17f15439f3cadcee898afbd98494bc3abb2b6f2
Emanuel4168 <emanuel.l4168@gmail.com>
2019-01-29 17:37:41 -0700

    Clase interna Empleados añadida

--- Registro Empleados/src/AplRegistroEmpleados.java
+++ Registro Empleados/src/AplRegistroEmpleados.java
@@ -1,4 +1,20 @@
 
 public class AplRegistroEmpleados {
+	
+	private class Empleado{
+		public String nombre;
+		public byte edad;
+		public float estatura;
+		
+		public Empleado(String nombre, byte edad, float estatura) {
+			this.nombre = nombre;
+			this.edad = edad; 
+			this.estatura = estatura;
+		}
+		
+		public String toString() {
+			return Rutinas.PonCeros(edad,3) + Rutinas.PonCeros((int) estatura, 3)+ Rutinas.PonBlancos(nombre, 30);
+		}
+	}
 
 }
