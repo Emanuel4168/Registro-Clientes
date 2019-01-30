ac2956e23f0afb0fb529054649cad14c3c919cea
Emanuel4168 <emanuel.l4168@gmail.com>
2019-01-29 17:56:38 -0700

    Métodos para depliegue del menú añadidos

--- Registro Empleados/src/AplRegistroEmpleados.java
+++ Registro Empleados/src/AplRegistroEmpleados.java
@@ -15,6 +15,28 @@ public class AplRegistroEmpleados {
 		public String toString() {
 			return Rutinas.PonCeros(edad,3) + Rutinas.PonCeros((int) estatura, 3)+ Rutinas.PonBlancos(nombre, 30);
 		}
+		
+	}
+	
+	public AplRegistroEmpleados() {
+		
+	}
+	
+	public static void main (String[] Args) {
+		new AplRegistroEmpleados();
+	}
+	
+	private void displayMainMenu() {
+		System.out.print("1.Elegir criterio de ordenamiento\n"+
+						 "2.Capturar empleado\n"+
+				         "3.Consultar");
+	}
+	
+	private void displaySubMenu() {
+		System.out.print("1.Nombre\n"+
+				 "2.Edad\n"+
+		         "3.Estatura\n"+
+				 "4.Edad-Estatura-Nombre");
 	}
 
 }
