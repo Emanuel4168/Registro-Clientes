d46550b7ce1f8db986077d5e52dbfa9bf55771e3
Emanuel4168 <emanuel.l4168@gmail.com>
2019-01-30 20:31:12 -0700

    Clases Lista y Nodo reemplazadas por ListaDBL y NodoDBL

--- Registro Empleados/src/AplRegistroEmpleados.java
+++ Registro Empleados/src/AplRegistroEmpleados.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class AplRegistroEmpleados {
 	
@@ -13,13 +14,44 @@ public class AplRegistroEmpleados {
 		}
 		
 		public String toString() {
-			return Rutinas.PonCeros(edad,3) + Rutinas.PonCeros((int) estatura, 3)+ Rutinas.PonBlancos(nombre, 30);
+			if(criterioOrdenamiento == 1)
+				return nombre;
+			if(criterioOrdenamiento == 2)
+				return Rutinas.PonCeros(edad,3);
+			if(criterioOrdenamiento == 3)
+				return Rutinas.PonBlancos(""+estatura,3);
+			
+			return Rutinas.PonCeros(edad,3) + Rutinas.PonBlancos(""+estatura, 3)+ Rutinas.PonBlancos(nombre, 30);
 		}
 		
 	}
 	
+	private static byte criterioOrdenamiento = 1; 
+	
 	public AplRegistroEmpleados() {
+		Scanner scan = new Scanner(System.in);
+		ListaDBL<Empleado> empleados = new ListaDBL<>();
+		byte opcion = 1;
 		
+		while(opcion != 0) {
+			displayMainMenu();
+			opcion = scan.nextByte();
+			
+			switch(opcion) {
+			case 1:
+				displaySubMenu();
+				criterioOrdenamiento = scan.nextByte();
+				scan.nextLine();
+				break;
+				
+			case 2:
+				empleados.InsertaOrdenado(guardarEmpleado(scan));
+				break;
+				
+			case 3: 
+				break;
+			}
+		}
 	}
 	
 	public static void main (String[] Args) {
@@ -27,16 +59,61 @@ public class AplRegistroEmpleados {
 	}
 	
 	private void displayMainMenu() {
-		System.out.print("1.Elegir criterio de ordenamiento\n"+
+		System.out.println("1.Elegir criterio de ordenamiento\n"+
 						 "2.Capturar empleado\n"+
 				         "3.Consultar");
 	}
 	
 	private void displaySubMenu() {
-		System.out.print("1.Nombre\n"+
+		System.out.println("1.Nombre\n"+
 				 "2.Edad\n"+
 		         "3.Estatura\n"+
 				 "4.Edad-Estatura-Nombre");
 	}
+	
+	private Empleado guardarEmpleado(Scanner scan) {
+		Empleado empleado = new Empleado("",(byte) 0,0);
+		System.out.println("Nombre:");
+		empleado.nombre = scan.nextLine();
+		System.out.println("Edad:");
+		empleado.edad = scan.nextByte();
+		scan.nextLine();
+		System.out.println("Estatura:");
+		empleado.estatura = scan.nextFloat();
+		scan.nextLine();
+		
+		return empleado;
+	}
+	
+/*	private void ordenarListaPorCriterio(Lista<Empleado> empleados, int lowerIndex, int higherIndex) {
+		int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        int pivot = lowerIndex+(higherIndex-lowerIndex)/2;
+        Nodo<Empleado> nodoPivote = empleados.getFrente(), nodoIzq= empleados.getFrente(), nodoDer=empleados.getFin();
+        // Divide into two arrays
+        while (i <= j) {
+        	
+            while (nodoIzq.toString().compareToIgnoreCase(nodoPivote.toString()) < 0) {
+                i++;
+                nodoIzq = nodoIzq.getSig();
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchangeNumbers(i, j);
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+        	ordenarListaPorCriterio(empleados, lowerIndex, j);
+        if (i < higherIndex)
+        	ordenarListaPorCriterio(empleados, i, higherIndex);
+		
+	}*/
 
 }
